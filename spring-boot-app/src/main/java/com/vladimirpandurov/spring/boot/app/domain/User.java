29aaf8848512c41d9ae6ef1ed7083837412d2915
panvla <pandvlado@gmail.com>
2023-03-19 21:18:43 +0100

    entity added.

---
+++ spring-boot-app/src/main/java/com/vladimirpandurov/spring/boot/app/domain/User.java
@@ -0,0 +1,25 @@
+package com.vladimirpandurov.spring.boot.app.domain;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String status;
+    private String address;
+    private String phone;
+    private String imageUrl;
+}
